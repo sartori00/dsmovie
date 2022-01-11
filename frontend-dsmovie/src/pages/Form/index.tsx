951c188aba5b15eb90e7c8a52ddcea98d02dcf05
Rodrigo Sartori <93sartori@gmail.com>
2022-01-11 20:41:00 -0300

    Navigation buttons

--- frontend-dsmovie/src/pages/Form/index.tsx
+++ frontend-dsmovie/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 export default function Form() {
@@ -35,7 +36,9 @@ export default function Form() {
             </button>
           </div>
         </form>
-        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        <Link to="/">
+          <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        </Link>
       </div>
     </div>
   );
