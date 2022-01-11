c5370a13841d7d84db8035609c36f2c986eb4ff2
Rodrigo Sartori <93sartori@gmail.com>
2022-01-11 20:04:50 -0300

    Routes

---
+++ frontend-dsmovie/src/pages/Form/index.tsx
@@ -0,0 +1,7 @@
+export default function Form() {
+  return (
+    <div>
+      <h1>Pagina de Form</h1>
+    </div>
+  );
+}
---
+++ frontend-dsmovie/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+export default function Listing() {
+  return (
+    <div>
+      <h1>Pagina de Listagem</h1>
+    </div>
+  );
+}
