28773acdd3dfe87621b3f55591b30b5493aac9f1
Rodrigo Sartori <93sartori@gmail.com>
2022-01-14 21:15:00 -0300

    First Request

--- frontend-dsmovie/src/pages/Listing/index.tsx
+++ frontend-dsmovie/src/pages/Listing/index.tsx
@@ -1,7 +1,15 @@
+import axios from 'axios';
 import MovieCard from 'components/MovieCard';
 import Pagination from 'components/Pagination';
+import { useEffect } from 'react';
+import { BASE_URL } from 'utils/requests';
 
 export default function Listing() {
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+      console.log(response.data);
+    });
+  }, []);
   return (
     <>
       <Pagination />
